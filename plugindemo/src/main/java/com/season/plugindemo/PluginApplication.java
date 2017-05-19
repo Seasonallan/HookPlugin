b124a461e490674ecab3d95b58201df61f20714b
451360508@qq.com <451360508@qq.com>
2017-05-19 17:18:52 +0800

    添加AIDL不同进程对数据的访问

---
+++ plugindemo/src/main/java/com/season/plugindemo/PluginApplication.java
@@ -0,0 +1,29 @@
+package com.season.plugindemo;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.io.File;
+
+/**
+ * Disc:
+ * User: SeasonAllan(451360508@qq.com)
+ * Time: 2017-05-16 10:07
+ */
+public class PluginApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e("SeasonLog", "PluginApplication onCreate");
+
+        File file = getFilesDir();
+        Log.e("SeasonLog", "FileDir = " + file.getAbsolutePath());
+        Log.e("SeasonLog", "FileDir exists = " + file.exists());
+
+        Fresco.initialize(this);
+
+    }
+}
